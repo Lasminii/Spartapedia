4ee75eac513de2ab89975db2591af3265178d66c
Lasminii <143819676+Lasminii@users.noreply.github.com>
2023-10-11 08:15:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def movie_post():
 
     og_image = soup.select_one('meta[property="og:image"]')
     og_title = soup.select_one('meta[property="og:title"]')
-    og_description = soup.select_one('meta[property="og:description"]')
+    og_description = soup.select_one('meta[name="description"]')
 
     image = og_image['content']
     title = og_title['content']
